8062afcc244926c6d39251468494838e1b7d7e8f
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-11-27 17:53:02 -0500

    done

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -4,12 +4,22 @@ class DogHousesController < ApplicationController
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
+    # dog_house = DogHouse.find(params[:dog_house_id])
+    # reviews = dog_house.reviews
+    # render json: reviews, include: :dog_house
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
   end
 
   private
 
   def render_not_found_response
+    # render json: { error: "Dog house not found" }, status: :not_found
     render json: { error: "Dog house not found" }, status: :not_found
+
   end
 
 end
